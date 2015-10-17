fa6cd296da166dd130eb574c1c6d1625114a5d24
rajnibebo <ubhi.rajni800@gmail.com>
2015-10-17 16:39:43 +0530

    This is the first commit to localhome

---
+++ TIJ/src/holdingobjects/AsListInference.java
@@ -0,0 +1,46 @@
+package holdingobjects;
+import java.util.*;
+
+class Snow {
+	public String toString() {
+		return "Snow";
+	}
+}
+class Powder extends Snow {
+	public String toString() {
+		return "Powder";
+	}
+}
+class Light extends Powder {
+	public String toString() {
+		return "Light";
+	}
+}
+class Heavy extends Powder {
+	public String toString() {
+		return "Heavy";
+	}
+}
+class Crusty extends Snow {
+	public String toString() {
+		return "Crusty";
+	}
+}
+class Slush extends Snow {
+	public String toString() {
+		return "Slush";
+	}
+}
+
+class AsListInference {
+	public static void main (String[] args) {
+		List<Snow> list = Arrays.asList(new Crusty(), new Slush(),new Powder());
+		System.out.println("List Got is "+list);
+		// if your jdk version is 8 , then following lines will compile but not otherwise
+		List<Snow> anotherList = Arrays.asList(new Light(),new Heavy());
+		System.out.println("Another list received is : "+anotherList);
+		
+		List<Snow> thirdList = new ArrayList<Snow>();
+		Collections.addAll(thirdList,new Light(),new Heavy());
+	}
+}
