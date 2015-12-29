3fa9ce82d8447b27b67dc8b8874f37ead61d7b58
rajnibebo <ubhi.rajni800@gmail.com>
2015-12-29 21:37:45 +0530

    Adding new pattern observerPattern

---
+++ observerpattern/src/com/headfirst/ch2/DisplayElement.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+package com.headfirst.ch2;
+
+/**
+ * @author Rajni
+ *
+ * @date 11-Jan-2015 12:18:39 am
+ */
+public interface DisplayElement {
+	public void display();
+}
---
+++ observerpattern/src/com/headfirst/ch2/Observer.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+package com.headfirst.ch2;
+
+/**
+ * @author Rajni
+ *
+ * @date 11-Jan-2015 12:07:58 am
+ */
+public interface Observer {
+	public void update(float temp,float pressure,float humidity);
+}
